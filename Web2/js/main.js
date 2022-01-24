ee3c04a4a4fa1edb20793f8b51302f7d74d72bfd
vladverheij <vladverheij@gmail.com>
2022-01-24 09:24:38 +0100

    .

--- Web2/js/main.js
+++ Web2/js/main.js
@@ -20,5 +20,5 @@ function showSlides() {
     slideIndex++;
     if (slideIndex > slides.length) { slideIndex = 1 }
     slides[slideIndex - 1].style.display = "block";
-    setTimeout(showSlides, -10000000); // Change image every 2 seconds
+    setTimeout(showSlides, 3000); // Change image every 3 seconds
 }
